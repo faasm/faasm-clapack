65382c0bb05d4fb74e233220905d1c653afaa74e
Simon Shillaker <scs17@ic.ac.uk>
2020-09-30 10:47:11 +0000

    Fix return type bug in s_copy

--- third-party/clapack/F2CLIBS/libf2c/s_copy.c
+++ third-party/clapack/F2CLIBS/libf2c/s_copy.c
@@ -11,10 +11,12 @@ extern "C" {
 
 /* assign strings:  a = b */
 
+// NOTE - s_copy returns int in clapack
+
 #ifdef KR_headers
 VOID s_copy(a, b, la, lb) register char *a, *b; ftnlen la, lb;
 #else
-void s_copy(register char *a, register char *b, ftnlen la, ftnlen lb)
+int s_copy(register char *a, register char *b, ftnlen la, ftnlen lb)
 #endif
 {
 	register char *aend, *bend;
@@ -51,6 +53,8 @@ void s_copy(register char *a, register char *b, ftnlen la, ftnlen lb)
 		while(a < aend)
 			*a++ = ' ';
 		}
+
+    return 0;
 	}
 #ifdef __cplusplus
 }
